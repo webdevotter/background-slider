5bcca1e911bf1701372e5f14259d16281b4ca79d
Larry Rhodes <81366018+webdevotter@users.noreply.github.com>
2022-09-28 11:09:28 -0500

    call background image

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const body = document.body
 const slides = document.querySelectorAll('.slide')
 const leftBtn = document.getElementById('left')
 const rightBtn = document.getElementById('right')
+
+let activeSlide = 0
+
+setBgToBody()
+
+function setBgToBody() {
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
